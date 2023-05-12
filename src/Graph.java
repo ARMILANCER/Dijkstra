ad5b4dde621ca819c4ecb6282c0be59e13b07930
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-05-13 01:08:49 +0200

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,60 +1,49 @@
 import java.util.*;
-
 public class Graph {
     private Set<Node> graph = new HashSet<>();
 
     public Set<Node> getGraph() {
         return graph;
     }
-
+    // new node to the graph
     public void addHop(Node hop){
         graph.add(hop);
     }
+
+    // Dijkstra
     public void shortestPath(Node genesis){
         genesis.setWeight(0);
         Set<Node> settledNodes = new HashSet<>();
-        Set<Node> unsettledNodes = new HashSet<>();
+        PriorityQueue<Node> unsettledNodes = new PriorityQueue<>(2, (a, b) -> a.getWeight() - b.getWeight());
         unsettledNodes.add(genesis);
         while (!unsettledNodes.isEmpty()){
             Node current = distance(unsettledNodes);
-            System.out.println("prima di unsettle"+current.getLinks().size());
             unsettledNodes.remove(current);
-            System.out.println(unsettledNodes);
-            System.out.println("dopo di unsettle"+current.getLinks().size()+"curr"+current);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            settledNodes.add(current);
             current.getLinks().forEach((node,weight)->{
                 if(!settledNodes.contains(node)){
-                    minPath(node,current);
-                    System.out.println("siii"+ node);
+                    minPath(node,weight,current);
                     unsettledNodes.add(node);
-                    System.out.println(" first if  "+unsettledNodes.size());
                 }
             });
         }
     }
 
-    private Node distance(Set<Node> unsettledNodes){
-        Comparator<Node> comp = (a, b) -> a.getWeight() - b.getWeight();
-        Node minimumWeight = new Node("temp");
-        minimumWeight.setWeight(Integer.MAX_VALUE);
-        //unsettledNodes.forEach((b)->{
+    private Node distance(PriorityQueue<Node> unsettledNodes){
+        Node minimumWeight = null;
+        int weight = Integer.MAX_VALUE;
         for (Node b: unsettledNodes) {
-            if (comp.compare(b, minimumWeight) < 0) {
+            if(b.getWeight()<weight){
                 minimumWeight = b;
-                System.out.println("Distance");
+                weight = b.getWeight();
             }
         }
         return minimumWeight;
     }
 
-    private void minPath(Node nextHop,Node current){
-        int sum = current.getWeight()+ nextHop.getWeight();
+    private void minPath(Node nextHop,int weight,Node current){
+        int sum = current.getWeight()+weight;
         if(sum < nextHop.getWeight());{
-            System.out.println("in minPath");
             nextHop.setWeight(sum);
             LinkedList<Node> path = new LinkedList<>(current.getMinPath());
             path.add(current);
